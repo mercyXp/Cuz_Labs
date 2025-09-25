c6e81e07c9507f62de22c6b46b9b500157d12e32
MercyXp <munzenzimercy2024@gmail.com>
2025-09-25 11:49:17 +0200

    Filter numbers greater than 50

---
+++ IT112_Programming/Lists/Part-B/question_4.py
@@ -0,0 +1,11 @@
+numbers = [12, 67, 45, 89, 34, 100, 23]
+
+# Create a new list with numbers greater than 50
+greater_than_50 = []
+
+for num in numbers:
+    if num > 50:
+        greater_than_50.append(num)
+
+print("Numbers greater than 50:", greater_than_50)
+
