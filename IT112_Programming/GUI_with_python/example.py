0e595d79852d8f201defac81387378b3f0891626
MercyXp <munzenzimercy2024@gmail.com>
2025-10-26 16:39:01 +0200

    demo: basic structure of a tkinter program

---
+++ IT112_Programming/GUI_with_python/example.py
@@ -0,0 +1,9 @@
+import tkinter     #import the tkinter module
+
+top = tkinter.Tk()  #create the main appplication window
+
+top.title("Welcome!:)")  #add widgets
+
+top.geometry("400x300") #size of the main window
+
+top.mainloop() #call the main event loop
